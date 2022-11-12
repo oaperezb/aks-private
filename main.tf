36d6a13eff4efe96e5266b3697e7e775ba3491f7
Omar Arturo Pérez Baqueiro <43418629+oaperezb@users.noreply.github.com>
2022-11-11 18:04:30 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+  client_id       = var.CLIENT_ID
+  client_secret   = var.CLIENT_SECRET
+  tenant_id       = var.TENANT_ID
+  subscription_id = var.SUBSCRIPTION_ID
+  skip_provider_registration = true
 }
 
 terraform {
